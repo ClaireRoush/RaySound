abc4420e24727d1694517afa8062c87af59edac2
claireroush <claireroush13iq@gmail.com>
2025-06-13 20:22:52 +0200

    refac: musicManager refactoring

--- music/playMusic.go
+++ music/playMusic.go
@@ -19,54 +19,51 @@ type Music struct {
 	Ext         string
 }
 type MusicManager struct {
-	queue []Music
+	queue []*Music
 	Index uint16
 }
 
 func (m *MusicManager) AddItem(path string) {
 	bytes, ext, err := utils.SaveCoverFromSong(path)
+	musicStream := rl.LoadMusicStream(path)
 	if err != nil {
 		fmt.Print(err)
 	}
-	item := Music{
+	item := &Music{
 		Title:       filepath.Base(path),
 		artist:      "meowArtist",
 		Path:        path,
 		length:      3200.0,
 		isPlaying:   false,
-		MusicStream: rl.LoadMusicStream(path),
+		MusicStream: musicStream,
 		Cover:       string(bytes),
 		Ext:         ext,
 	}
 	m.queue = append(m.queue, item)
 }
 
-func (m *MusicManager) GetItem(index uint16) Music {
-	if int(index) >= len(m.queue) {
-		return Music{}
-	}
-	return m.queue[index]
+func (m *MusicManager) GetSongRn() *Music {
+	return m.queue[m.Index]
 }
 
-func (m *MusicManager) GetItems() []Music {
+func (m *MusicManager) GetItems() []*Music {
 	return m.queue
 }
 
+func (m *MusicManager) GetItem(index uint32) *Music {
+	items := m.GetItems()
+	if len(items) < int(index) && index < 0 {
+		return &Music{}
+	}
+	return items[index]
+}
+
 func (m *MusicManager) PlayMusic(index uint16) {
-	if int(m.Index) < len(m.queue) && m.queue[m.Index].isPlaying {
-		rl.StopMusicStream(m.queue[m.Index].MusicStream)
-		m.queue[m.Index].isPlaying = false
-	}
-	if int(index) >= len(m.queue) {
-		println("Invalid index:", index)
-		return
-	}
-	m.queue[index].MusicStream = rl.LoadMusicStream(m.queue[index].Path)
-	rl.PlayMusicStream(m.queue[index].MusicStream)
-	m.queue[index].isPlaying = true
+	songs := m.GetItems()
+	rl.PlayMusicStream(songs[index].MusicStream)
 	m.Index = index
 }
 
 func (m *MusicManager) UpdateStream() {
-	rl.UpdateMusicStream(m.queue[m.Index].MusicStream)
+	rl.UpdateMusicStream(m.GetSongRn().MusicStream)
 }
